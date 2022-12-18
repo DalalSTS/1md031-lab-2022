6aa996bff6c93d68af1abc3ef54eed61bd0737ee
DalalSTS <dalal.alhafez1998@gmail.com>
2022-12-18 03:19:01 +0100

    Last-1

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
       <div class="box a">
+        <p> Your nummber of orders is: {{amountOrdered}}</p>
             <h3>{{burger.name}}</h3>
             <!-- v-bind:src= -->
             <img v-bind:src="burger.url" style="width:15em; height:20em">
@@ -9,6 +10,9 @@
               <li>Contains <span class="certain">{{burger.lactose}}</span></li>
               <li>Contains <span class="certain">{{burger.gluten}}</span></li>
             </ul>
+
+            <button type="button" v-on:click="addBurger">+Add</button>
+            <button type="button" v-on:click="removeBurger">Remove-</button>
           </div>
     </div>
   </template>
@@ -18,7 +22,31 @@
     name: 'OneBurger',
     props: {
       burger: Object
-    }
+    },
+    data: function () {
+      return {
+        amountOrdered: 0,
+      }
+    },
+
+    methods: {
+    addBurger: function () {
+      this.amountOrdered += 1;
+      this.$emit('orderedBurger', { name:   this.burger.name,
+            amount: this.amountOrdered
+          }
+      );
+    },
+    removeBurger: function () {
+      if (this.amountOrdered > 0){
+      this.amountOrdered -= 1;
+      this.$emit('orderedBurger', { name:   this.burger.name,
+            amount: this.amountOrdered
+          }
+      );
+        }
+    },
+  }
   }
   </script>
   
